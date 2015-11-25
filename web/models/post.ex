c1b060d17211bf4b28e2ada482a28cb236ee1f50
Mickael Remond <mremond@process-one.net>
2015-11-25 16:07:05 +0100

    Add exemple of changeset validation

--- web/models/post.ex
+++ web/models/post.ex
@@ -21,5 +21,6 @@ defmodule Gastronokids.Post do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> validate_length(:name, min: 3, max: 30)
   end
 end
